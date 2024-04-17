b89cadbe07bf561fd8ed897d2a612867fb71ad7c
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-04-17 20:21:40 +0200

    Steam: Opened firewall for remoteplay and added gamemode

--- modules/steam.nix
+++ modules/steam.nix
@@ -3,9 +3,12 @@
 {
   programs.steam = {
     enable = true;
+    remotePlay.openFirewall = true;
+
     package = pkgs.steam.override {
       extraPkgs = pkgs: with pkgs; [
         gamescope
+        gamemode
         mangohud
         protontricks
       ];
