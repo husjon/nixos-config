30d121d89b5fccf983951147217f8dc21c17f3a5
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-06-04 22:02:03 +0200

    Added cache.husjon.xyz
    * added host cache.husjon.xyz
    
    * added initial nginx setup
    
    * added /alpine/ endpoint
    
    * added /archlinux/ endpoint
    
    * added /debian/ and /debian-secure/ endpoint
    
    * added /raspbian/ endpoint
    
    * added /nixos/ endpoint
    
    * added localhost to nix.settings.substituters

---
+++ modules/cache.husjon.xyz/8030-raspbian.nix
@@ -0,0 +1,38 @@
+{ ... }:
+
+{
+  services.nginx = {
+    virtualHosts."cache" = {
+
+      locations."~ ^/raspbian/(.*?\.deb)$" = {
+        extraConfig = ''
+          proxy_pass        http://raspbian/$1;
+          proxy_cache       STATIC;
+          proxy_cache_valid 200 4w;
+        '';
+      };
+      locations."~ ^/raspbian/(.*?(InRelease|Release|Packages(.gz|.xz)?))$" = {
+        extraConfig = ''
+          proxy_pass        http://raspbian/$1;
+          proxy_cache       STATIC;
+          proxy_cache_valid 200 1h;
+        '';
+      };
+      locations."~ ^/raspbian/(.*?)$" = {
+        extraConfig = ''
+          proxy_pass        http://raspbian/$1;
+          proxy_cache       STATIC;
+          proxy_cache_valid 200 1h;
+        '';
+      };
+    };
+
+    upstreams."raspbian" = {
+      servers = { "127.0.0.1:8030" = { }; };
+    };
+    virtualHosts."raspbian" = {
+      listen = [{ addr = "127.0.0.1"; port = 8030; }];
+      locations."/" .proxyPass = "http://archive.raspberrypi.org/debian$request_uri";
+    };
+  };
+}
