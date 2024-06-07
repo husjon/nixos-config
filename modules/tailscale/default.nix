c6add0323b4013ce4289b376b1a9901fdd0c85af
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-06-07 18:48:39 +0200

    enabled authKey for tailscale

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ config, ... }:
 
 {
+  sops.secrets.tailscale_auth_key = { };
+
   services.tailscale = {
     enable = true;
+
+    # https://login.tailscale.com/admin/settings/keys
+    authKeyFile = config.sops.secrets.tailscale_auth_key.path;
   };
 }
