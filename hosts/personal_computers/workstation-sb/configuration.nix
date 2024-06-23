6ec877c0ae34d3adaf124b3f762d273e8c2c31ea
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-06-23 16:01:22 +0200

    configure locale

--- hosts/personal_computers/workstation-sb/configuration.nix
+++ hosts/personal_computers/workstation-sb/configuration.nix
@@ -38,18 +38,6 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nb_NO.UTF-8";
-    LC_IDENTIFICATION = "nb_NO.UTF-8";
-    LC_MEASUREMENT = "nb_NO.UTF-8";
-    LC_MONETARY = "nb_NO.UTF-8";
-    LC_NAME = "nb_NO.UTF-8";
-    LC_NUMERIC = "nb_NO.UTF-8";
-    LC_PAPER = "nb_NO.UTF-8";
-    LC_TELEPHONE = "nb_NO.UTF-8";
-    LC_TIME = "nb_NO.UTF-8";
-  };
-
   # Configure keymap in X11
   services.xserver = {
     xkb = {
