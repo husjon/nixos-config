86076bd5455cfe258a72bf99a9c6337f71f9d8b4
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-08-16 00:01:03 +0200

    updated microcode on workstation

--- hosts/personal_computers/workstation/hardware-configuration.nix
+++ hosts/personal_computers/workstation/hardware-configuration.nix
@@ -61,5 +61,5 @@
   # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
