af24b724d5b86574ef5be00b511dc5f9f4e2dc9c
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-08-24 15:06:23 +0200

    added cachix substituter for hyprland

--- modules/window_manager/hyprland.nix
+++ modules/window_manager/hyprland.nix
@@ -17,6 +17,12 @@
     ];
   };
 
+  # https://wiki.hyprland.org/Nix/Cachix/
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
