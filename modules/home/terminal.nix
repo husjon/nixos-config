ffe5a88bf094cc4d037b129c56650b54b59fe701
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-08-28 22:26:43 +0200

    added some keybinds to kitty

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -13,6 +13,13 @@
       enable_audio_bell = false;
 
       shell = "${pkgs.fish}/bin/fish";
+
+    };
+
+    keybindings = {
+      "ctrl+plus" = "change_font_size all +2.0";
+      "ctrl+minus" = "change_font_size all -2.0";
+      "ctrl+0" = "change_font_size all 0";
     };
   };
 }
