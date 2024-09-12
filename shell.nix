edc5889da44e418de4b8d8d9c0c92b2eed18ce5a
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-09-12 20:39:38 +0200

    added helper script to deploy to remote

--- shell.nix
+++ shell.nix
@@ -27,5 +27,43 @@ pkgs.mkShell {
     (writeShellScriptBin "rebuild-switch" ''
       sudo nixos-rebuild --flake ".#" switch
     '')
+
+    (writeShellScriptBin "rebuild-remote" ''
+      #!/usr/bin/env bash
+
+      case ''$1 in
+          # Personal Computers
+          laptop|workstation|workstation-sb)
+              HOST=''$1
+              ;;
+          *)
+              echo "unknown target"
+              exit 1
+              ;;
+      esac
+
+
+      case ''$2 in
+          switch|test|dry-build)
+              OPERATION=''$2
+              ;;
+          *)
+              echo "unknown operation (supported: switch, test)"
+              exit 1
+              ;;
+      esac
+
+      if [ ''${HOST} = ''$(hostname) ]; then
+          echo 'Use "rebuild-test" or "rebuild-switch" instead'
+          exit 1
+      else
+          NIX_SSHOPTS="-o ControlPath=''$HOME/.ssh/controlmasters-%r@%h:22 -o ControlMaster=auto -o ControlPersist=10s " \
+          nixos-rebuild \
+              --build-host "root@''${HOST}" \
+              --target-host "root@''${HOST}" \
+              --flake ".#''${HOST}" \
+              ''${OPERATION}
+      fi
+    '')
   ];
 }
