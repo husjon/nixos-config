022b45ffeb5b148742716f6678ba6e9b181959e8
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-09-12 20:13:21 +0200

    enabled upower

--- modules/system/tlp.nix
+++ modules/system/tlp.nix
@@ -3,4 +3,12 @@
   services.tlp = {
     enable = true;
   };
+
+  services.upower = {
+    enable = true;
+
+    percentageLow = 15;
+    percentageCritical = 5;
+    criticalPowerAction = "Hibernate"; # one of "PowerOff", "Hibernate", "HybridSleep"
+  };
 }
