381228e2e0b7f4ba5d05b40cfe102edd59f57034
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-10-19 23:12:26 +0200

    enabled autoUpgrade

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -5,4 +5,11 @@
     ./tlp.nix
     ./ups.nix
   ];
+
+  system.autoUpgrade = {
+    enable = true;
+    allowReboot = true;
+    flake = "github:husjon/nixos-config";
+    dates = "04:00";
+  };
 }
