b01aea758497b91ede53ff48ed1d3cb4082d2639
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-10-30 22:09:43 +0100

    test setup using sway and greetd

---
+++ modules/window_manager/sway.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  programs.sway = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+      };
+    };
+  };
+}
