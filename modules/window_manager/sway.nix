bed9753281ad9324229dceb230a3042a21ef60b9
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-11-17 18:49:46 +0100

    configured xdg.portal.wlr

--- modules/window_manager/sway.nix
+++ modules/window_manager/sway.nix
@@ -5,6 +5,18 @@
     xwayland.enable = true;
   };
 
+  xdg.portal.wlr = {
+    enable = true;
+    settings = {
+      screencast = {
+        output_name = "HDMI-A-1";
+        max_fps = 30;
+        chooser_type = "simple";
+        chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+      };
+    };
+  };
+
   services.greetd = {
     enable = true;
     settings = {
