b0064e5c6d5827a0d81c189625b54899675f39a5
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-11-30 20:26:14 +0100

    disable focus on xwaylandvideobridge altogether

--- modules/home/wayland/hyprland/default.nix
+++ modules/home/wayland/hyprland/default.nix
@@ -363,7 +363,7 @@ in
         # xwaylandvideobridge {{{
         "opacity 0.0 override, class:^(xwaylandvideobridge)$"
         "noanim, class:^(xwaylandvideobridge)$"
-        "noinitialfocus, class:^(xwaylandvideobridge)$"
+        "nofocus, class:^(xwaylandvideobridge)$"
         "maxsize 1 1, class:^(xwaylandvideobridge)$"
         "noblur, class:^(xwaylandvideobridge)$"
         "float, class:^(xwaylandvideobridge)$"
