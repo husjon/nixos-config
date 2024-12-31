79e389d74fc76eb441f01b680ad4f24d3efad9aa
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-12-31 15:34:44 +0100

    chore(flake): update stable to 24.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Nix channel status: https://status.nixos.org/
     # https://discourse.nixos.org/t/differences-between-nix-channels/13998
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     sops-nix.url = "github:Mic92/sops-nix";
