023c43ed88d672734d3c9423d97ecbc08a1f6098
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2025-02-19 22:26:58 +0100

    chore(git): added global gitignore file
    primarily to ignore .borg-nobackup files

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,6 +4,16 @@
 
   programs.git = {
     enable = true;
+
+    extraConfig = {
+      core.excludesfile = "~/.gitignore";
+    };
+  };
+
+  home.file.".gitignore" = {
+    text = ''
+      .borg-nobackup
+    '';
   };
 
   programs.lazygit.enable = true;
