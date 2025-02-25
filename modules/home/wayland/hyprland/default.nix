19761b85091463c18701963dfca176167176e74a
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2025-02-25 08:56:23 +0100

    gcmsg "chore(hyprland): added exec for restarting the wayland service"

--- modules/home/wayland/hyprland/default.nix
+++ modules/home/wayland/hyprland/default.nix
@@ -163,7 +163,7 @@ in
 
       exec = [
         "hyprctl dispatch dpms on"
-        # "pkill waybar; sleep 0.1; ${pkgs.waybar}/bin/waybar"
+        "systemctl --user restart waybar"
         "pgrep -f .Discord-wrapped || ${pkgs.discord}/bin/discord"
         "pgrep -f '^xwaylandvideobridge$' || xwaylandvideobridge"
 
