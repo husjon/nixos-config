c41a91eeff2129f8e7d2458e2beb6ad0b45ce890
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2025-02-25 09:37:04 +0100

    fix(kitty): moved mouse bindings to extraConfig

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -21,12 +21,15 @@
       "ctrl+plus" = "change_font_size all +2.0";
       "ctrl+minus" = "change_font_size all -2.0";
       "ctrl+0" = "change_font_size all 0";
-
-      # disable left_click and shift+left_click for links
-      "left" = "click ungrabbed";
-      "shift+left" = "click ungrabbed";
-      # use ctrl+left_click for links
-      "ctrl+left" = "release grabbed,ungrabbed mouse_handle_click link";
     };
+
+    extraConfig = ''
+      # disable left_click and shift+left_click for links
+      mouse_map left click ungrabbed
+      mouse_map shift+left click ungrabbed
+      # use ctrl+left_click for links
+      mouse_map ctrl+left release grabbed,ungrabbed mouse_handle_click link
+    '';
+
   };
 }
