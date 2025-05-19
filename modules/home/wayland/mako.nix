0e64ac6d605f6ee89ef129a5c5d16f584531c440
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2025-05-19 13:43:31 +0200

    chore(mako): updated config to new syntax

--- modules/home/wayland/mako.nix
+++ modules/home/wayland/mako.nix
@@ -4,36 +4,39 @@
     mako = {
       enable = true;
 
-      height = 200;
-      width = 500;
+      settings = {
+        height = 200;
+        width = 500;
 
-      borderRadius = 4;
-      defaultTimeout = 5000;
-      anchor = "bottom-right";
+        border-radius = 4;
+        default-timeout = 5000;
+        anchor = "bottom-right";
 
-      backgroundColor = "#181825dd";
-      textColor = "#cdd6f4";
-      borderColor = "#89b4fa";
-      progressColor = "#313244";
+        background-color = "#181825dd";
+        text-color = "#cdd6f4";
+        border-color = "#89b4fa";
+        progress-color = "#313244";
 
-      extraConfig = ''
-        padding=16
-        margin=8,0
-        outer-margin=10
+        padding = 16;
+        margin = "8,0";
+        outer-margin = 10;
 
-        [urgency=low]
-        border-color=#585b70
-        text-color=#a6adc8
+        "urgency=low" = {
+          border-color = "#585b70";
+          text-color = "#a6adc8";
+        };
 
-        [urgency=high]
-        border-color=#f38ba8
-        text-color=#f38ba8
+        "urgency=high" = {
+          border-color = "#f38ba8";
+          text-color = "#f38ba8";
+        };
 
-        [app-name="Spotify"]
-        default-timeout=2000
-        text-color=#55ce6d
-        border-color=#55ce6d
-      '';
+        "app-name=Spotify" = {
+          default-timeout = 2000;
+          text-color = "#55ce6d";
+          border-color = "#55ce6d";
+        };
+      };
     };
   };
 }
