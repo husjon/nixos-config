382d0d096a2604ceab37f3169d05fa255140ac4d
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2025-07-22 11:08:48 +0200

    chore(flake.nix): changed nixpkgs-unstable.url to track nixpkgs-unstable instead of nixos-unstable
    making it the same as nixpkgs.url

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # https://discourse.nixos.org/t/differences-between-nix-channels/13998
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
