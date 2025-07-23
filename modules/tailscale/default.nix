225bacabd5d34c5e842dfe0aa5bf315b3de651b2
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2025-07-23 18:25:20 +0200

    chore(tailscale): added systemd service to restart after suspend and hibernation

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -9,4 +9,19 @@
     # https://login.tailscale.com/admin/settings/keys
     authKeyFile = config.sops.secrets.tailscale_auth_key.path;
   };
+
+  systemd.services."tailscale-post_hibernate_restart" = {
+    enable = true;
+
+    script = "systemctl restart tailscaled";
+
+    after = [
+      "hibernate.target"
+      "suspend.target"
+    ];
+    wantedBy = [
+      "hibernate.target"
+      "suspend.target"
+    ];
+  };
 }
