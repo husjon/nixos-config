ef41406bb2df2d094c4e5d00c566dd3e2010dba4
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2025-08-28 13:27:08 +0200

    feat(shell.nix): added rebuild-vm helper

--- shell.nix
+++ shell.nix
@@ -41,6 +41,14 @@ pkgs.mkShell {
       sudo nixos-rebuild --flake ".#" --no-update-lock-file switch
     '')
 
+    (writeShellScriptBin "rebuild-vm" ''
+      nixos-rebuild --flake ".#" --no-update-lock-file build-vm
+
+      QEMU_OPTS="-display gtk,grab-on-hover=on -smp cpus=4" \
+        ./result/bin/run-*-vm
+      rm ./result ./*.qcow2
+    '')
+
     (writeShellScriptBin "rebuild-remote" ''
       #!/usr/bin/env bash
 
