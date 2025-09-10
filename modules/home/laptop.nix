24d587547f595abbf35f4fc55ec6435e621b76ec
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2025-09-10 20:33:29 +0200

    chore(modules/programs/ncmpcpp): moved to new structure

--- modules/home/laptop.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-{
-  # List of options: https://nix-community.github.io/home-manager/options.xhtml
-  imports = [ ];
-
-  home.packages = with pkgs; [ ];
-}
---
+++ modules/programs/ncmpcpp.nix
@@ -0,0 +1,72 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.husjon.programs.ncmpcpp;
+in
+{
+  options.husjon.programs.ncmpcpp = lib.mkEnableOption "ncmpcpp";
+
+  config = lib.mkIf cfg.enable {
+    programs.ncmpcpp = {
+      enable = true;
+
+      settings = {
+        media_library_primary_tag = "album_artist";
+        volume_change_step = 1;
+      };
+      bindings = [
+        {
+          key = "j";
+          command = "scroll_down";
+        }
+        {
+          key = "k";
+          command = "scroll_up";
+        }
+        {
+          key = "h";
+          command = "previous_column";
+        }
+        {
+          key = "l";
+          command = "next_column";
+        }
+        {
+          key = "ctrl-b";
+          command = "page_up";
+        }
+        {
+          key = "ctrl-u";
+          command = "page_up";
+        }
+        {
+          key = "ctrl-f";
+          command = "page_down";
+        }
+        {
+          key = "ctrl-d";
+          command = "page_down";
+        }
+        {
+          key = "g";
+          command = "move_home";
+        }
+        {
+          key = "G";
+          command = "move_end";
+        }
+        {
+          key = "n";
+          command = "next_found_item";
+        }
+        {
+          key = "N";
+          command = "previous_found_item";
+        }
+      ];
+    };
+  };
+}
