90f682a58f54e8a16d00e79d4783c0d9bc7d9f60
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2025-10-29 23:26:16 +0100

    chore(mako): removed colors

--- modules/programs/mako.nix
+++ modules/programs/mako.nix
@@ -24,25 +24,13 @@ in
             ignore-timeout = 1;
             anchor = "bottom-right";
 
-            background-color = "#181825dd";
-            text-color = "#cdd6f4";
-            border-color = "#89b4fa";
-            progress-color = "#313244";
-
             padding = 16;
             margin = "8,0";
             outer-margin = 10;
 
             layer = "overlay";
 
-            "urgency=low" = {
-              border-color = "#585b70";
-              text-color = "#a6adc8";
-            };
-
             "urgency=high" = {
-              border-color = "#f38ba8";
-              text-color = "#f38ba8";
               default-timeout = 0;
             };
 
