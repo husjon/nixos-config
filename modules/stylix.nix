16e47c1c95785c516d25b611d798535be66b7b46
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2025-10-30 21:44:21 +0100

    chore(stylix): set font types and sizes

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -5,6 +5,32 @@
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   stylix.polarity = "dark";
 
+  stylix.fonts = {
+    sizes = {
+      applications = 12;
+      desktop = 12;
+      popups = 12;
+      terminal = 11;
+    };
+
+    emoji = {
+      name = "Symbols Nerd Font";
+      package = pkgs.nerd-fonts.symbols-only;
+    };
+    monospace = {
+      name = "FiraCode Nerd Font Mono";
+      package = pkgs.nerd-fonts.fira-mono;
+    };
+    serif = {
+      name = "FiraCode Nerd Font";
+      package = pkgs.nerd-fonts.fira-code;
+    };
+    sansSerif = {
+      name = "FiraCode Nerd Font";
+      package = pkgs.nerd-fonts.fira-code;
+    };
+  };
+
   # NixOS options goes here (example: chromium.enable)
   # https://nix-community.github.io/stylix/options/platforms/nixos.html
   stylix.targets = {
