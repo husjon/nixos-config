53f743225124167c33d9dd372da4170d3abc8b76
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2025-11-20 12:48:50 +0100

    chore(filemanager): replaced file-roller option with package

--- modules/filemanager/default.nix
+++ modules/filemanager/default.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  cfg = config.husjon;
+in
 {
   programs.thunar.enable = true;
   programs.thunar.plugins = with pkgs.xfce; [
@@ -7,10 +10,15 @@
   ];
   programs.xfconf.enable = true; # For persisting changes preferences (since xfce is not used as DM)
   services.gvfs.enable = true; # Mount, trash, and other functionalities
-  programs.file-roller.enable = true; # For managing Archives
   services.tumbler.enable = true; # Thumbnail support for images
 
   xdg.mime.defaultApplications = {
     "inode/directory" = [ "thunar.desktop" ];
+  };
+
+  home-manager.users = {
+    "${cfg.user.username}".home.packages = with pkgs; [
+      file-roller
+    ];
   };
 }
